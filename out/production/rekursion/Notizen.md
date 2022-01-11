28c7971880c2a098e4d3374742892a4e0dec5ca1
StrawberryMilk05 <tobias.schaelte@outlook.de>
2022-01-11 07:35:56 +0100

    init commit

---
+++ out/production/rekursion/Notizen.md
@@ -0,0 +1,12 @@
+# Rekursion
+
+* Programme rufen sich slebst auf
+
+```
+void rek(){
+    rek();
+}
+```
+
+* Rekursion wird abgesetzt von der Iteration
+* Mithilfe der Rekursion kann man bestimmte Probleme sehr elegant lösen
--- src/Rekursion.java
+++ src/Rekursion.java
@@ -1,3 +1,38 @@
 public class Rekursion {
 
+    // Endlosrekursion /vgl. Endlosschleife)
+    // In der Praxis ist die Rekursionstiefe von der Größe des Stacks abhängig
+    // mit anderen Worten: Rekursion bricht ab, wenn der Stapel voll ist.
+    private static void rekursion01(){
+        rekursion01();
+    }
+
+
+
+    private static void rekursion02(){
+        // Abbruchbedingung
+        // Trivialfall / Elementarfall ==> NICHT-rekursiver Fall
+        if(true) {
+            System.out.println("Keine Rekursion mehr");
+        } else {
+            rekursion02();
+        }
+    }
+
+
+
+    private static void rekursion03(int i){
+        // Abbruchbedingung
+        // Trivialfall / Elementarfall ==> NICHT-rekursiver Fall
+        if(i == 10) {
+            System.out.println("Stopp!");
+        } else {
+            System.out.println(i);
+            rekursion03(i+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        rekursion03(0);
+    }
 }
