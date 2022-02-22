60f6f0fb7a92e0a7f512ead989c30e62fbd726d6
StrawberryMilk05 <tobias.schaelte@outlook.de>
2022-02-22 07:49:21 +0100

    help added

--- src/Rekursion_Anordnung.java
+++ src/Rekursion_Anordnung.java
@@ -6,7 +6,7 @@ public class Rekursion_Anordnung {
         } else {
 
             for (int i = 0; i < todo.length(); i++) {
-                String rest = (i-1 >= 0 ? todo.substring(0,i) : "") + todo.substring(i+1);
+                String rest = todo.substring(0,i) + todo.substring(i+1);
                 char c = todo.charAt(i);
                 letterMixer(rest,finished+c);
             }
